18cc798ee00d5b7e8604524afea2e948a4e10c06
JavierReyes <javjreyes@gmail.com>
2022-12-28 11:32:34 -0600

    created class game, instructor, and class outline

---
+++ RPS.js
@@ -0,0 +1,22 @@
+class game{
+    constructor(){
+        //initialize game scores at the start of a new game
+        this.playerScore=0;
+        this.comScore=0;
+
+        //set choices to null
+        this.playerChoice=null;
+        this.comChoice=null;
+    }
+    
+    getComputerChoice(){}
+    playRound(){}
+
+}
+
+
+document.onload=init;
+
+function init(){
+
+}

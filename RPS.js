a9935a210997c6bd6554f1c6712bdba3bf83105d
JavierReyes <javjreyes@gmail.com>
2022-12-28 23:38:02 -0600

    rmv json package and impliment getComChoice+test

--- RPS.js
+++ RPS.js
@@ -1,22 +1,38 @@
-class game{
+class Game{
     constructor(){
         //initialize game scores at the start of a new game
         this.playerScore=0;
         this.comScore=0;
 
+        //initialize choice options
+        this.options=["rock","paper","scissors"];
         //set choices to null
         this.playerChoice=null;
         this.comChoice=null;
+        
     }
     
-    getComputerChoice(){}
-    playRound(){}
+    getComChoice(){
+        let choice= Math.floor(Math.random()*3);
+        return this.options[choice];
+    }
+    
+    playRound(){
+        
+    }
+
+    tests(){
+        console.log("getComChoice returns:",this.getComChoice())
+    }
 
 }
 
 
-document.onload=init;
+//document.onload=init;
 
 function init(){
-
+    let game=new Game();
+    game.tests();
 }
+//call init for testing
+init();

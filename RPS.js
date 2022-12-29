abc680efd2fd8eca0fbf3466dfe121c9bef5d78c
JavierReyes <javjreyes@gmail.com>
2022-12-29 00:42:08 -0600

    impliment winner method & added test for winner()

--- RPS.js
+++ RPS.js
@@ -12,17 +12,55 @@ class Game{
         
     }
     
+    //determine computer selection
     getComChoice(){
         let choice= Math.floor(Math.random()*3);
         return this.options[choice];
     }
-    
-    playRound(){
-        
+
+    //determine round winner
+    winner(com, player){
+        let inputs=[com,player];
+        if(com==null||player==null){
+            return "invalid";
+        }
+        else if(com===player){
+            return "draw";
+        }
+        else{
+            switch(inputs){
+                case ["rock","paper"]:
+                    return "player";
+                case ["rock","scissors"]:
+                    return "com";
+                case ["paper","rock"]:
+                    return "com";
+                case ["paper","scissors"]:
+                    return "player";
+                case["scissors","rock"]:
+                    return "player";
+                case["scissors","paper"]:
+                    return "com";
+            }
+        }
+
+
     }
 
+    //play round
+    playRound(){
+
+    }
+
+    //prints function outputs to console for testing
     tests(){
+        //test get com choice
         console.log("getComChoice returns:",this.getComChoice())
+
+        //test winner
+        console.log("winner(null, rock) returns", this.winner(null,"rock"));
+        console.log("winner(com==player) rerturns", this.winner("rock","rock"));
+        console.log("winner(rock, paper) returns:",this.winner("rock","paper"));
     }
 
 }
